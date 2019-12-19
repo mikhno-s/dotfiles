b990e748a268f134c06fa5626c66948b6d50bf38
mikhno.s <sergio.mikhno@gmail.com>
2019-12-19 23:18:54 +0200

    Add gitconfig

--- init.sh
+++ init.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-
-test -d $HOME/dotfiles || git clone git@github.com:mikhno-s/dotfiles.git $HOME/dotfiles
-git -C $HOME/dotfiles pull
-
-# TODO sync iterm setting and fonts
-
-# TODO sync oh-my-zsh settings 
-
-# Installing zsh plugings
-test -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions || git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-test -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting || git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 create_link() {
   if [[ -L $HOME/$1 ]]; then 
     rm $HOME/$1
@@ -22,6 +11,17 @@ create_link() {
 
   ln -s $HOME/dotfiles/$1 $HOME/$1
 }
+
+test -d $HOME/dotfiles || git clone git@github.com:mikhno-s/dotfiles.git $HOME/dotfiles
+git -C $HOME/dotfiles pull
+
+# TODO sync iterm setting and fonts
+
+# TODO sync oh-my-zsh settings 
+
+# Installing zsh plugings
+test -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions || git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+test -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting || git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 create_link ".vimrc"
 create_link ".zshrc"
