be00c3f8e8133688628e9db855046e22d6529a60
sm <sergio.mikhno@gmail.com>
2020-01-11 15:00:53 +0200

    Fix

--- init.sh
+++ init.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 create_link() {
-  if [[ -L $HOME/$1 ]]; then 
-    rm $HOME/$1
+  if [[ -L $HOME/"${1}" ]]; then 
+    rm $HOME/"${1}"
   fi
 
-  if [[ -f $HOME/$1 ]]; then
-    mv $HOME/$1 $HOME/dotfiles/$1.bak
+  if [[ -f $HOME/"${1}" ]]; then
+    mv $HOME/"${1}" $HOME/dotfiles/"${1}".bak
   fi
 
-  ln -s $HOME/dotfiles/$1 $HOME/$1
+  ln -s $HOME/dotfiles/"${1}" $HOME/"${1}"
 }
 
 test -d $HOME/dotfiles || git clone git@github.com:mikhno-s/dotfiles.git $HOME/dotfiles
@@ -26,4 +26,4 @@ create_link ".zshrc"
 create_link ".gitignore"
 create_link ".ssh/config"
 create_link ".gitconfig"
-create_link "Library/Application Support/iTerm2/DynamicProfiles/iterm_sm.json"
+create_link Library/Application\ Support/iTerm2/DynamicProfiles/iterm_sm.json
